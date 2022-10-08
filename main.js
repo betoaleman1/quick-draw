c5f8f067241d06831fc29f7ae909749c473705bb
betoaleman1 <105089286+betoaleman1@users.noreply.github.com>
2022-10-07 19:31:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
-
-
 function setup() {
-  canvas = createCanvas(280, 280);
-  canvas.position(500,215);
-  background("white");
-  canvas.mouseReleased(classifyCanvas);
-  synth = window.speechSynthesis;
-}
-
-function preload() {
-
-
-  classifier = ml5.imageClassifier('DoodleNet');
-}
-
-
-
-function clearCanvas() {
-
-  background("white");
-}
-
-function draw() {
-
-  // Establece el grosor del stroke (trazo) a 13.
-  strokeWeight(13);
-  // Establece el color del stroke (trazo) a negro.
-  stroke(0);
-  // Si el mouse está presionado, dibuja una línea entre la posición previa y la actual del mouse.
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
+    canvas = createCanvas(280, 280);
+    canvas.position(625,215);
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
   }
-}
-
-function classifyCanvas() {
-  classifier.classify(canvas, gotResult);
-}
-
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
+  
+  function preload() {
+  
+  
+    classifier = ml5.imageClassifier('DoodleNet');
   }
-  console.log(results);
-  document.getElementById('label').innerHTML = 'Etiqueta: ' + results[0].label;
-
-  document.getElementById('confidence').innerHTML = 'Confianza: ' + Math.round(results[0].confidence * 100) + '%';
-
-  utterThis = new SpeechSynthesisUtterance(results[0].label);
-  synth.speak(utterThis);
-}
-
-
+  
+  
+  
+  function clearCanvas() {
+  
+    background("white");
+  }
+  
+  function draw() {
+  
+    // Establece el grosor del stroke (trazo) a 13.
+    strokeWeight(13);
+    // Establece el color del stroke (trazo) a negro.
+    stroke(0);
+    // Si el mouse está presionado, dibuja una línea entre la posición previa y la actual del mouse.
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  }
+  
+  function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Etiqueta: ' + results[0].label;
+  
+    document.getElementById('confidence').innerHTML = 'Confianza: ' + Math.round(results[0].confidence * 100) + '%';
+  
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+  }
+  
+  
+  
